3de648aaac0503f8712639adb210d9bca9572777
Divyansh Vaish <rksrivastava9890@gmail.com>
2025-07-08 16:15:56 +0530

    done

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,14 @@
-// db.js
 const mongoose = require("mongoose");
-
-const MONGODB_URI =
-  "mongodb+srv://rksrivastava9890:UlSY7YNEgR6Q6jE2@centuryfinancecluster.bggmzok.mongodb.net/equiLearnDB?retryWrites=true&w=majority&appName=CenturyFinanceCluster";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB connected ✅");
   } catch (err) {
-    console.error("MongoDB connection failed ❌", err);
+    console.error("MongoDB connection error ❌", err);
     process.exit(1);
   }
 };
